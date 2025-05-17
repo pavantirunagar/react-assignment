f4f8cf6a677022a91d06ac2b51217dabd9352a97
Pavan <pavantirunagar11514@gmail.com>
2025-05-17 17:49:10 +0530

    fix ("build") deployment issue

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@mui/material';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../redux/store';
